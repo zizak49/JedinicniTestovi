c52448afeda99e1299dc5a97979596c068e8207e
Julijan Šribar <julijan.sribar@gmail.com>
2017-02-28 19:39:22 +0100

    Initial implementation

---
+++ KompleksniBroj/KompleksniBroj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom
+{
+    public struct KompleksniBroj
+    {
+        public KompleksniBroj(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public double RealniDio;
+        public double ImaginarniDio;
+    }
+}
---
+++ Testovi/TestKompleksnogBroja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom;
+
+namespace Testovi
+{
+    [TestClass]
+    public class TestKompleksnogBroja
+    {
+        [TestMethod]
+        public void PozivKonstruktoraInicijaliziraRealniDioKompleksnogBroja()
+        {
+            KompleksniBroj kb = new KompleksniBroj(1, 4);
+            Assert.AreEqual(1, kb.RealniDio);
+        }
+
+        [TestMethod]
+        public void PozivKonstruktoraInicijaliziraImaginarniDioKompleksnogBroja()
+        {
+            KompleksniBroj kb = new KompleksniBroj(1, 4);
+            Assert.AreEqual(4, kb.ImaginarniDio);
+        }
+    }
+}
