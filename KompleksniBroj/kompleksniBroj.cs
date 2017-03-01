39e880a49e5a37ce89ecf9a291210e1e1a202bb9
Mislav Franjo <mislav.franja@gmail.com>
2017-03-01 21:08:18 +0100

    Gotov

---
+++ KompleksniBroj/kompleksniBroj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom
+{
+    public struct kompleksniBroj
+    {
+        public kompleksniBroj(double realni,double imaginarni){
+            Realni = realni;
+            Imaginarni = imaginarni;
+        }
+        static public kompleksniBroj operator +(kompleksniBroj br1, kompleksniBroj br2) {
+            return new kompleksniBroj(br1.Realni + br2.Realni, br1.Imaginarni + br2.Imaginarni);
+        }
+        static public kompleksniBroj operator -(kompleksniBroj br1, kompleksniBroj br2)
+        {
+            return new kompleksniBroj(br1.Realni - br2.Realni, br1.Imaginarni - br2.Imaginarni);
+        }
+
+        public double Realni;
+        public double Imaginarni;
+    }
+}
---
+++ Testovi/testKompleksnogBroja.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom;
+
+namespace Testovi
+{
+    /// <summary>
+    /// Summary description for testKompleksnogBroja
+    /// </summary>
+    [TestClass]
+    public class testKompleksnogBroja
+    {
+        
+
+        [TestMethod]
+        public void PozivKonstruktoraKompleksnogBrojaKaoRealniDioPostavljenog()
+        {
+            kompleksniBroj kb = new kompleksniBroj(4, 7);
+            Assert.AreEqual(4, kb.Realni);
+        }
+        [TestMethod]
+        public void PozivKonstruktoraKompleksnogBrojaKaoImagniarniDioPostavljenog()
+        {
+            kompleksniBroj kb = new kompleksniBroj(4, 7);
+            Assert.AreEqual(7, kb.Imaginarni);
+        }
+        [TestMethod]
+        public void Zbroj2KomleksnaBrojaRealniIO()
+        {
+            kompleksniBroj kb1 = new kompleksniBroj(4, 7);
+            kompleksniBroj kb2 = new kompleksniBroj(7, 9);
+
+            Assert.AreEqual(11, (kb1+kb2).Realni);
+        }
+        [TestMethod]
+        public void Zbroj2KomleksnaBrojaImaginarniDio()
+        {
+            kompleksniBroj kb1 = new kompleksniBroj(4, 7);
+            kompleksniBroj kb2 = new kompleksniBroj(7, 9);
+
+            Assert.AreEqual(16, (kb1+kb2).Imaginarni);
+        }
+
+
+    }
+}
